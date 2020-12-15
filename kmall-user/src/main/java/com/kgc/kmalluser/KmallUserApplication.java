d3742595667ff84f8f3acd8572ea08f1b88fcdb8
shangfucheng_glb <shangfucheng_glb@kgc.com>
2020-12-15 10:45:08 +0800

    上传kmall-user

---
+++ kmall-user/src/main/java/com/kgc/kmalluser/KmallUserApplication.java
@@ -0,0 +1,13 @@
+package com.kgc.kmalluser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KmallUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KmallUserApplication.class, args);
+    }
+
+}
---
+++ kmall-user/src/test/java/com/kgc/kmalluser/KmallUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kgc.kmalluser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KmallUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
